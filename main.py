1dccf61373aab5eed679418dda0b5cc1dffd0496
Ari E <133052948+aqesters@users.noreply.github.com>
2023-09-25 21:54:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ Last Modified: 25 Sept 2023
 """
     
 # extract data
-filepath = st.file_uploader("Upload your transcations CSV file from your TD Ameritrade account", type='csv')
+filepath = st.file_uploader("Upload your transcations CSV file from your TD Ameritrade account :point_down:", type='csv')
 
 if filepath is not None:
     st.write(filepath)

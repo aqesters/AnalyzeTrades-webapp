05ca62826ea0658a13fb8949108cf61257282298
Ari E <133052948+aqesters@users.noreply.github.com>
2023-09-26 13:33:03 -0700

    Update Timeframe.py

--- Timeframe.py
+++ Timeframe.py
@@ -7,30 +7,14 @@ Created on Fri Sep  8 22:25:27 2023
 """
 import datetime as dt
 
-# Input: 
-    # "dates" input contains datetime objects arranged in ascending order
-    # user input: to sum up over the last __ days
-# Returns: starting index corresponding to the last __ days
+# Inputs: 
+    # dates == contains datetime objects arranged in ascending order
+    # N == user input, the script will summarize data over the last N days
+# Returns: starting index corresponding to the last N days
 
-def Timeframe(dates):
-    # get user input for number of days
-    allowed = False
-    err = "ERROR: Number of days must be a whole number greater than zero (1,2,3...)"
-    pastdays = input("---> Sum up trades over the last how many days? Enter here: ")
-    while allowed == False:
-        try:
-            pastdays = int(pastdays)
-            allowed = (pastdays >= 1)
-        except ValueError or TypeError:  # input is not a number
-            print(err)
-            pastdays = input("---> Sum up trades over the last how many days? Enter here: ")
-            continue
-        if allowed == False:  # input is zero or negative number
-            print(err)
-            pastdays = input("---> Sum up trades over the last how many days? Enter here: ")
-    
+def Timeframe(dates, N):    
     # find index that matches desired timeframe
-    delta = dt.timedelta(pastdays)
+    delta = dt.timedelta(N)
     lastdate = dates[-1]
     startdate = lastdate - delta
     for i in range(len(dates)):

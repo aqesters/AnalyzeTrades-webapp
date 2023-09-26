b9bc2c228ad7e3a9cc0a7855210c43a51807baf5
Ari E <133052948+aqesters@users.noreply.github.com>
2023-09-26 14:41:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,6 @@ uploaded_file = st.file_uploader("Upload your transcations CSV file from your TD
 
 if uploaded_file is not None:
     file_bytes = uploaded_file.getvalue().decode("utf-8")
-    st.write(file_bytes)
-    st.stop()
     trades = ExtractData(file_bytes)
     st.write(len(trades))
     st.write(trades)

286955d8c72c702322ce7bdeff7dfb44bffc612b
Ari E <133052948+aqesters@users.noreply.github.com>
2023-09-26 14:49:51 -0700

    Update ExtractData.py

--- ExtractData.py
+++ ExtractData.py
@@ -8,39 +8,27 @@ Created on Fri Sep  8 22:07:44 2023
 
 import re 
 import csv
-from io import StringIO
 # Input:
-    # 'filepath' == path to transactions CSV file uploaded by user
+    # 'file' == bytes object containing CSV file uploaded by user
 # Returns:
     # 'trades' == raw trading data from input CSV file
     # 'filedir' == directory of the input CSV file
     
 def ExtractData(file):
-    '''
-    # get the directory for this file, save data here later
-    parts = re.split('/', filepath)
-    i = -1 * len(parts[-1])  # index right before filename
-    filedir = filepath[:i]
-    '''
     
     # import data from file
-    print("Extracting...")
-    #with open(file, newline='') as csvfile:
     contents = csv.reader(file.splitlines())
     linenum = 1
-    headers = []
     trades = []
     
-    '''# iterate through each row
-    for row in contents:
+    # iterate through each row
+    for row in list(contents):
         if linenum == 1:  # first row has headers
-            headers = row
+            continue
         else:
             trades.append(row) # add row to list of trades
         linenum += 1
         
     # ignore last line, which says "END OF FILE"
-    trades.pop(-1)'''
-
-    print("Extraction complete!")    
-    return list(contents)
+    trades.pop(-1)
+    return trades

0c57c6baf47910b7777b0143d76ee4ef3f1146ea
Ari E <133052948+aqesters@users.noreply.github.com>
2023-09-26 14:01:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -37,58 +37,41 @@ if csvfile is not None:
     # process data
     dates, tickers, amounts, options, comms, fees, symbols, prices, signedQty = ProcessData(trades)
     
-    # Query and present data within retry mechanism
-    retry = 'Y'
-    while retry == 'Y':
-        # user chooses timeframe
-        start = Timeframe(dates)
-        
-        # isolate date from timeframe and plot
-        d = dates[start:]
-        #t = tickers[start:]
-        #a = amounts[start:]
-        o = options[start:]
-        c = comms[start:]
-        f = fees[start:]
-        s = symbols[start:]
-        p = prices[start:]
-        q = signedQty[start:]
-        
-        # sum up commissions and fees
-        commsum = SumString(c)
-        feesum = SumString(f)
+    # user chooses timeframe
+    all_days = dates[-1] - dates[0]
+    ndays = st.slider(1, all_days, 30)
+    start = Timeframe(dates, ndays)
     
-        # Write trade journal for all closed trades
-        # STRUCTURE = [date open, date close, ticker, price open, price close, quantity, P/L, P/L %]
-        closedpos = CreateJournal(d, s, p, q, o, commsum, feesum, filedir)
-        
-        # Plot data
-        date1, date2, amountsum = PlotData(closedpos, filedir)
-        
-        # Summarize data
-        grandsum = amountsum - commsum - feesum      
-        print("\nSUMMARY (" + date1 + " - " + date2 + ")")
-        print(line)
-        print("Trade Profit-Loss (ignores P/L Open): ${:.2f}".format(amountsum))
-        print("Total Commissions: ${:.2f}".format(commsum))
-        print("Total Fees: ${:.2f}".format(feesum))
-        print("Net Total: ${:.2f}".format(grandsum))
-        print(line)
-        print("Note: Only positions that were both opened and closed during this timeframe are counted.")
-        
-        # let user decide whether to retry
-        validAns = False
-        while validAns is False:
-            print("\nPlots will be shown for this timeframe if you're done.")
-            ans = input("---> Would you like to try a different timeframe? Enter here (Y/N): ")
-            if (ans.upper() == 'Y') or (ans.upper() == 'YES'):
-                validAns = True
-                retry = 'Y'
-                continue
-            elif (ans.upper() == 'N') or (ans.upper() == 'NO'):
-                validAns = True
-                retry = 'N'
-                print('Goodbye!')
-                time.sleep(1)
-            else:
-                print('Answer not recognized. Please try again.')
+    # isolate date from timeframe and plot
+    d = dates[start:]
+    #t = tickers[start:]
+    #a = amounts[start:]
+    o = options[start:]
+    c = comms[start:]
+    f = fees[start:]
+    s = symbols[start:]
+    p = prices[start:]
+    q = signedQty[start:]
+    
+    # sum up commissions and fees
+    commsum = SumString(c)
+    feesum = SumString(f)
+
+    # Write trade journal for all closed trades
+    # STRUCTURE = [date open, date close, ticker, price open, price close, quantity, P/L, P/L %]
+    closedpos = CreateJournal(d, s, p, q, o, commsum, feesum, filedir)
+    
+    # Plot data
+    #date1, date2, amountsum = PlotData(closedpos, filedir)
+    
+    # Summarize data
+    grandsum = amountsum - commsum - feesum  
+    line = "-----------------------------------"
+    st.write("SUMMARY (", date1, " - ", date2, ")")
+    st.write(line)
+    st.write("Trade Profit-Loss (ignores P/L Open): ${:.2f}".format(amountsum))
+    st.write("Total Commissions: ${:.2f}".format(commsum))
+    st.write("Total Fees: ${:.2f}".format(feesum))
+    st.write("Net Total: ${:.2f}".format(grandsum))
+    st.write(line)
+    st.write("Note: Only positions that were both opened and closed during this timeframe are counted.")
